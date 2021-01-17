f0e363a9b64334d88b0567e95a9a010a8144533d
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-01-17 19:09:22 +0100

    Fixes on tests

--- test/conftest.py
+++ test/conftest.py
@@ -6,7 +6,7 @@ from typing import Dict
 import sys
 
 
-current_dir = Path( sys.argv[0] if __name__ == "__main__" else __file__  ).resolve()
+current_dir = Path( sys.argv[0] if __name__ == "__main__" else __file__  ).parent.resolve()
 
 @pytest.fixture(scope="session")
 def root_repo_dir() -> Path:
--- test/test_workflows.py
+++ test/test_workflows.py
@@ -1,20 +1,22 @@
-#
-# TODO: 'User-Agent': 'osparc-api/{packageVersion}/{language}'
-#
-#
+# pylint:disable=unused-variable
+# pylint:disable=unused-argument
+# pylint:disable=redefined-outer-name
 
+import datetime
 import os
+import time
 from pathlib import Path
 from pprint import pformat, pprint
 from typing import List
 
-import osparc
-import osparc.exceptions as errors
 import packaging.version as pv
 import pytest
+
+import osparc
+import osparc.exceptions as errors
 from osparc.api.files_api import FilesApi
 from osparc.configuration import Configuration
-from osparc.models import FileUploaded, Meta, Solver, Job, JobState
+from osparc.models import FileUploaded, Job, JobState, Meta, Solver
 from osparc.rest import ApiException
 
 
@@ -26,9 +28,7 @@ def as_dict(obj: object):
     }
 
 
-
-
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def api_client(project_environ_patched):
     cfg = Configuration(
         host=os.environ.get("OSPARC_API_URL", "http://127.0.0.1:8000"),
@@ -41,22 +41,23 @@ def api_client(project_environ_patched):
         yield api_client
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def meta_api(api_client):
     return osparc.MetaApi(api_client)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def files_api(api_client):
     return osparc.FilesApi(api_client)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def solvers_api(api_client):
     return osparc.SolversApi(api_client)
 
 
 # ----------
+
 
 def test_get_service_metadata(meta_api):
     print("get Service Metadata", "-" * 10)
@@ -70,10 +71,10 @@ def test_get_service_metadata(meta_api):
     assert status_code == 200
 
 
-def test_upload_single_file(files_api):
-    # some file
-    input_path = Path("tmp-input.txt")
+def test_upload_single_file(files_api, tmpdir):
+    input_path = Path(tmpdir) / "some-text-file.txt"
     input_path.write_text("demo")
+
     input_file: FileUploaded = files_api.upload_single_file(file=input_path)
     assert isinstance(input_file, FileUploaded)
 
@@ -84,10 +85,12 @@ def test_upload_single_file(files_api):
     assert input_file.hash == same_file.hash
 
 
-def test_upload_list_download(files_api: FilesApi):
-    input_path = Path("tmp-input.h5")
-    input_path.write_bytes(b"demo")
+def test_upload_list_download(files_api: FilesApi, tmpdir):
+    input_path = Path(tmpdir) / "some-hdf5-file.h5"
+    input_path.write_bytes(b"demo but some other stuff as well")
+
     input_file: FileUploaded = files_api.upload_single_file(file=input_path)
+    assert isinstance(input_file, FileUploaded)
 
     assert input_file.filename == input_path.name
 
@@ -99,40 +102,43 @@ def test_upload_list_download(files_api: FilesApi):
     assert input_path.read_text() == same_file
 
 
-
 def test_solvers(solvers_api):
     solvers: List[Solver] = solvers_api.list_solvers()
-    
+
     latest = None
     for solver in solvers:
         if "isolve" in solver.name:
-            if latest and pv.parse(latest.version) < pv.parse(solver.version):
+            if not latest:
+                latest = solver
+            elif pv.parse(latest.version) < pv.parse(solver.version):
                 latest = solvers_api.get_solver_by_id(solver.uuid)
 
     print(latest)
     assert latest
 
     assert (
-        solvers_api.get_solver_by_name_and_version(solver_name="isolve", version="latest")
+        solvers_api.get_solver_by_name_and_version(
+            solver_name=latest.name, version="latest"
+        )
         == latest
     )
     assert solvers_api.get_solver_by_id(latest.uuid) == latest
 
-import datetime
-import time
-
 
 def test_run_solvers(solvers_api):
-    solver = solvers_api.get_solver_by_name_and_version(solver_name="isolve", version="latest")
+
+    solver = solvers_api.get_solver_by_name_and_version(
+        solver_name="simcore/services/comp/isolve", version="latest"
+    )
     assert isinstance(solver, Solver)
 
     #
-    # Why creating a job and not just running directly from solver? 
+    # Why creating a job and not just running directly from solver?
     # Adding this intermediate step allows the server to do some extra checks before running a job.
     # For instance, does user has enough resources left? If not, the job could be rejected
     #
 
-    # I would like to run a job with my solver and these inputs. 
+    # I would like to run a job with my solver and these inputs.
     # TODO: how to name the body so we get nice doc?
     job = solvers_api.create_job(solver.uuid, job_input=[])
 
@@ -145,50 +151,43 @@ def test_run_solvers(solvers_api):
 
     # gets jobs granted for user with a given solver
     solver_jobs = solvers_api.list_jobs(solver.uuid)
-    assert solver_jobs == [job, ]
-    
+    assert solver_jobs == [
+        job,
+    ]
+
     # I only have jobs from this solver ?
     all_jobs = solvers_api.list_all_jobs()
     assert all_jobs == solver_jobs
 
-
     # let's run the job
     submit_time = datetime.utcnow()
-    state = solvers_api.start_job(job.job_id) 
+    state = solvers_api.start_job(job.job_id)
     assert isinstance(state, JobState)
 
     assert state.status == "PENDING"
     assert state.progress == 0
-    assert submit_time < state.submitted_at < submit_time + datetime.timedelta(seconds=1)
+    assert (
+        submit_time < state.submitted_at < submit_time + datetime.timedelta(seconds=1)
+    )
 
-
+    # TODO: progress could be per output?
+    #  - progress 100 * number-of-outputs
     while state.progress != 100:
         time.sleep(1)
         state = solvers_api.inspect_job(job.job_id)
         print("Solver progress", f"{state.progress}/100 completed")
-    
+
     # done
     assert state.progress == 100
     assert state.status == ["SUCCESS", "FAILED"]
     assert state.submitted_at < state.started_at
     assert state.started_at < state.stopped_at
 
-    # let's get the resouts
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # let's get the results
+    #  - each result might be ready to get at different times
+    # TODO: ?????
+    # outputs = solvers_api.list_job_outputs(job.job_id)
+    # for output in outputs:
+    #    if output.ready:
+    #        result = solvers_api.get_job_output(job.job_id, output.id)
+    #        assert
