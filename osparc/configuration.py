5675f5b5163399de4e4ebfbda5f3a3efc7aeb8ac
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-02-02 21:01:06 +0100

    Reverted to 0.3.10

--- osparc/configuration.py
+++ osparc/configuration.py
@@ -336,7 +336,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.4.0\n"\
-               "SDK Package Version: 0.4.0".\
+               "SDK Package version: 0.3.10".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
