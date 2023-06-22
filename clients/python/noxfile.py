ac6ec074f744f64f8c6914ff92cee5528b0de6b0
Mads Bisgaard <bisgaard@itis.swiss>
2023-06-22 11:36:14 +0200

    readd noxfile.py

---
+++ clients/python/noxfile.py
@@ -0,0 +1,18 @@
+import nox
+
+nox.options.stop_on_first_error = True
+nox.options.reuse_existing_virtualenvs = True
+
+source_files = ("osparc", "test", "setup.py", "noxfile.py")
+
+
+@nox.session(python=["3.6", "3.7", "3.8", "3.9"])
+def test(session):
+    session.install("-r", "requirements-tests.txt")
+    session.install("-e", ".")
+
+    options = session.posargs
+    if "-k" in options or "-x" in options:
+        options.append("--no-cov")
+
+    session.run("pytest", "-v", *options)
