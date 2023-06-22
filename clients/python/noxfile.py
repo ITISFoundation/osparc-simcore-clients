5d1b055094a74474354687ea7f173e5cd9f03b10
Mads Bisgaard <bisgaard@itis.swiss>
2023-06-22 13:11:59 +0200

    filter correct tests in noxfile

--- clients/python/noxfile.py
+++ clients/python/noxfile.py
@@ -1,10 +1,12 @@
 import nox
+from pathlib import Path
 
+
+py_dir = Path(__file__).parent.resolve()
 nox.options.stop_on_first_error = True
 nox.options.reuse_existing_virtualenvs = True
 
 source_files = ("osparc", "test", "setup.py", "noxfile.py")
-
 
 @nox.session(python=["3.6", "3.7", "3.8", "3.9"])
 def test(session):
@@ -15,4 +17,4 @@ def test(session):
     if "-k" in options or "-x" in options:
         options.append("--no-cov")
 
-    session.run("pytest", "-v", *options)
+    session.run("pytest", "-v", f"--ignore={py_dir/'auto_generated_client'}",*options)
