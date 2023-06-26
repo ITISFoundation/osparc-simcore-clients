91c4a25f2eb973b71b55143702fe7bd35b176964
Mads Bisgaard <bisgaard@itis.swiss>
2023-06-26 16:06:45 +0200

    check for gh config dir

--- scripts/gh.bash
+++ scripts/gh.bash
@@ -5,13 +5,17 @@
 
 
 USER_DIR=$(realpath ~)
-
+GH_CONFIG_DIR=${USER_DIR}/config/gh
 source $(dirname "$0")/gh/config.bash
 
 if [[ -n "$GITHUB_ACTIONS" ]]; then
   echo "Running in GitHub Actions"
   gh "$@"
 else
-  docker run --rm --volume="${USER_DIR}/config/gh":"/root/config/gh" \
+  if [ ! -d "${GH_CONFIG_DIR}" ]; then
+      mkdir -p "${GH_CONFIG_DIR}"
+      echo "Directory created: ${GH_CONFIG_DIR}"
+  fi
+  docker run --rm --volume="${GH_CONFIG_DIR}":"/root/config/gh" \
     ${IMAGE_NAME}:${IMAGE_VERSION} "$@"
 fi
