5a771e67fae1f90af89518f582ae40fbfaaa790c
Mads Bisgaard <bisgaard@itis.swiss>
2023-06-28 15:22:52 +0200

    changes to tests

--- clients/python/noxfile.py
+++ clients/python/noxfile.py
@@ -14,5 +14,14 @@ def test(session):
     if "-k" in options or "-x" in options:
         options.append("--no-cov")
 
-    session.install("--find-links=/artifacts/dist", "*")
+    dist_dir = Path(__file__).parent / 'artifacts' / 'dist'
+    if not dist_dir.is_dir():
+        raise RuntimeError(f'Could not find {dist_dir}. Did you forget to generate the client wheels?')
+
+    dir_content = dist_dir.glob('*')
+    osparc_wheels = list(dist_dir.glob('*osparc-*.whl'))
+    if len(osparc_wheels) != 1:
+        raise RuntimeError(f'Could not find osparc_wheel. It is not in {dist_dir}: {dir_content}.')
+
+    session.install(osparc_wheels[0], "--find-links", dist_dir)
     session.run("pytest", "-v", f"--ignore={py_dir/'artifacts/client'}",*options)
