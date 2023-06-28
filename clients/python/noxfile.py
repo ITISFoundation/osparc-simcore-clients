e285c82e6cfc3c109b1795692a86e11257c64833
Mads Bisgaard <bisgaard@itis.swiss>
2023-06-28 14:38:36 +0200

    install client in nox file

--- clients/python/noxfile.py
+++ clients/python/noxfile.py
@@ -6,7 +6,6 @@ py_dir = Path(__file__).parent.resolve()
 nox.options.stop_on_first_error = True
 nox.options.reuse_existing_virtualenvs = True
 
-source_files = ("osparc", "test", "setup.py", "noxfile.py")
 
 @nox.session(python=["3.6", "3.7", "3.8", "3.9"])
 def test(session):
@@ -15,4 +14,5 @@ def test(session):
     if "-k" in options or "-x" in options:
         options.append("--no-cov")
 
+    session.install("--find-links=/artifacts/dist", "*")
     session.run("pytest", "-v", f"--ignore={py_dir/'artifacts/client'}",*options)
