81e84786983284e8e07205410b76c913aae96ae0
Mads Bisgaard <bisgaard@itis.swiss>
2023-06-29 10:33:57 +0200

    test fix

--- clients/python/test_osparc/test_basic.py
+++ clients/python/test_osparc/test_basic.py
@@ -5,5 +5,5 @@ def test_osparc_client():
     from osparc import osparc_client
 
 def test_get_api():
-    info = osparc.get_api()
+    info = osparc.info.get_api()
     assert isinstance(info['info']['version'],str)
