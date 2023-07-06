3add9edaea586e196c4c20f6df7385e92c7a8929
Mads Bisgaard <bisgaard@itis.swiss>
2023-07-06 22:41:52 +0200

    add further sanity checks

--- clients/python/test/e2e/test_notebooks.py
+++ clients/python/test/e2e/test_notebooks.py
@@ -16,6 +16,7 @@ def test_notebook_config():
     config_test_nb:Path = Path(__file__).parent / 'data' / 'config_test.ipynb'
     assert config_test_nb.is_file()
     test_run_notebooks(config_test_nb,{'expected_python_bin':sys.executable})
+    assert len(all_notebooks) > 0, f'Did now find any notebooks in {docs_dir}'
 
 @pytest.mark.parametrize('notebook,params',list(zip(all_notebooks,[{}]*len(all_notebooks))))
 def test_run_notebooks(notebook:Path, params:Dict[str,Any]):
