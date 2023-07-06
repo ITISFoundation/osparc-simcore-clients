cc8e41e6ac474239a714c68d9239413ddc5c9b5b
Mads Bisgaard <bisgaard@itis.swiss>
2023-07-06 22:43:42 +0200

    minor change

--- clients/python/test/e2e/test_notebooks.py
+++ clients/python/test/e2e/test_notebooks.py
@@ -9,14 +9,13 @@ import sys
 docs_dir:Path = Path(__file__).parent.parent.parent / 'docs'
 all_notebooks:List[Path] = list(docs_dir.glob("*.ipynb"))
 
-
 def test_notebook_config():
     """ Checks the jupyter environment is configured correctly
     """
     config_test_nb:Path = Path(__file__).parent / 'data' / 'config_test.ipynb'
     assert config_test_nb.is_file()
     test_run_notebooks(config_test_nb,{'expected_python_bin':sys.executable})
-    assert len(all_notebooks) > 0, f'Did now find any notebooks in {docs_dir}'
+    assert len(all_notebooks) > 0, f'Did not find any notebooks in {docs_dir}'
 
 @pytest.mark.parametrize('notebook,params',list(zip(all_notebooks,[{}]*len(all_notebooks))))
 def test_run_notebooks(notebook:Path, params:Dict[str,Any]):
