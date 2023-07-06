ecb5dcb4d2bb84b09169f55e72725da9150d1e31
Mads Bisgaard <bisgaard@itis.swiss>
2023-07-06 10:34:57 +0200

    small changes to notebook test

--- clients/python/test_osparc/test_notebooks.py
+++ clients/python/test_osparc/test_notebooks.py
@@ -2,19 +2,22 @@ import papermill as pm
 from pathlib import Path
 from tempfile import TemporaryDirectory
 import shutil
+import pytest
+from typing import List
+
+docs_dir:Path = Path(__file__).parent.parent / 'docs'
+all_notebooks:List[Path] = list(docs_dir.glob("*.ipynb"))
 
 
-def test_run_notebooks():
+@pytest.mark.parametrize('notebook',all_notebooks)
+def test_run_notebooks(notebook:Path):
     """Run all notebooks in the documentation
     """
-    docs_dir:Path = Path(__file__).parent.parent / 'docs'
-    assert docs_dir.is_dir()
-    for nb in docs_dir.glob("*.ipynb"):
-        print(f"Testing {nb.relative_to(docs_dir)}")
-        assert nb.is_file(), f'{nb.relative_to(docs_dir)} is not a file'
-        with TemporaryDirectory() as tmp_dir:
-            tmp_nb = Path(tmp_dir) / nb.name
-            shutil.copy(nb, tmp_nb)
-            assert tmp_nb.is_file(), 'Did not succeed in copying notebook'
-            output:Path = Path(tmp_dir) / (tmp_nb.stem + '_output.ipynb')
-            pm.execute_notebook(input_path=tmp_nb, output_path=output, kernel_name="python")
+    print(f"Running {notebook.relative_to(docs_dir)}")
+    assert notebook.is_file(), f'{notebook.relative_to(docs_dir)} is not a file'
+    with TemporaryDirectory() as tmp_dir:
+        tmp_nb = Path(tmp_dir) / notebook.name
+        shutil.copy(notebook, tmp_nb)
+        assert tmp_nb.is_file(), 'Did not succeed in copying notebook'
+        output:Path = Path(tmp_dir) / (tmp_nb.stem + '_output.ipynb')
+        pm.execute_notebook(input_path=tmp_nb, output_path=output, kernel_name="python")
