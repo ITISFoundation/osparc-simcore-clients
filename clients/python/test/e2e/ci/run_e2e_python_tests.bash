4f4cf0cf30e9476cb25a4c4d10b378f4df3d24a8
Mads Bisgaard <bisgaard@itis.swiss>
2023-07-13 13:19:03 +0200

    document setup script for installing osparc client

--- clients/python/test/e2e/ci/run_e2e_python_tests.bash
+++ clients/python/test/e2e/ci/run_e2e_python_tests.bash
@@ -7,16 +7,16 @@ set -o pipefail # don't hide errors within pipes
 CI_DIR=$(realpath "$(dirname "$0")")
 E2E_DIR=$(realpath "${CI_DIR}/..")
 
-unset CLIENT_CONFIG
-unset SERVER_CONFIG
+unset OSPARC_CLIENT_CONFIG
+unset OSPARC_SERVER_CONFIG
 
 while getopts ":c:s:" arg; do
   case $arg in
     c) # Define client configuration
-      CLIENT_CONFIG="${OPTARG}"
+      OSPARC_CLIENT_CONFIG="${OPTARG}"
       ;;
     s) # Define server configuration
-      SERVER_CONFIG="${OPTARG}"
+      OSPARC_SERVER_CONFIGS="${OPTARG}"
       ;;
     *)
       echo "Received unknown flag"
@@ -25,12 +25,14 @@ while getopts ":c:s:" arg; do
   esac
 done
 
-#bash "${SCRIPTS_DIR}"/install_ci_osparc_python_client.bash -r "${CLIENT_REPO}" -b "${CLIENT_BRANCH}" -v "${CLIENT_VERSION}"
-NSCONFIG=$(echo "${SERVER_CONFIG}" | jq length)
+OSPARC_CLIENT_CONFIG=$(bash "${CI_DIR}"/setup_client_config.bash "${OSPARC_CLIENT_CONFIG}")
+
+
+NSCONFIG=$(echo "${OSPARC_SERVER_CONFIGS}" | jq length)
 for (( ii=0; ii<NSCONFIG; ii++ ))
 do
-    SCONFIG=$(echo "${SERVER_CONFIG}" | jq .[${ii}] )
-    python "${CI_DIR}"/setup_e2e_pytest.py "${CLIENT_CONFIG}" "${SCONFIG}"
+    SCONFIG=$(echo "${OSPARC_SERVER_CONFIGS}" | jq .[${ii}] )
+    python "${CI_DIR}"/setup_e2e_pytest.py "${OSPARC_CLIENT_CONFIG}" "${SCONFIG}"
     (
       # run in subshell to ensure env doesnt survive
       pytest "${E2E_DIR}" -p env
