9c656e53e3d88b37e8d1c1f1517b3ce3ae6bae10
Mads Bisgaard <bisgaard@itis.swiss>
2023-08-03 11:14:36 +0200

    add pagination->iterator method

--- clients/python/client/osparc/_solvers_api.py
+++ clients/python/client/osparc/_solvers_api.py
@@ -1,12 +1,15 @@
-from typing import Any, Iterator
+from typing import Any, Iterator, List
 
 from osparc_client import SolversApi as _SolversApi
 
 from . import ApiClient, Job
+from ._utils import _pagination_to_iterator
 
 
 class SolversApi:
     """Class for interacting with solvers"""
+
+    _wrapped_methods: List[str] = ["get_jobs"]
 
     def __init__(self, api_client: ApiClient):
         """Construct object
@@ -27,8 +30,13 @@ class SolversApi:
         Yields:
             Iterator[Job]: An iterator whose elements are the Jobs submitted to the solver
         """
-        limit: int = 20
-        offset: int = 0
+        pagination_method = lambda limit, offset: self._solvers_api.get_jobs_page(
+            solver_key=solver_key, version=version, limit=limit, offset=offset
+        )
+        return _pagination_to_iterator(pagination_method=pagination_method)
 
-    def __getattr__(self, __name: str) -> Any:
-        return getattr(self._solvers_api, __name)
+    def __getattr__(self, name: str) -> Any:
+        if name in self._wrapped_methods:
+            return getattr(self, name)
+        else:
+            return getattr(self._solvers_api, name)
--- clients/python/client/osparc/_utils.py
+++ clients/python/client/osparc/_utils.py
@@ -1,5 +1,46 @@
-from typing import Any, Callable, Iterator
+from typing import Any, Callable, Iterator, TypeVar, Union
+
+from osparc_client import (
+    File,
+    Job,
+    LimitOffsetPageFile,
+    LimitOffsetPageJob,
+    LimitOffsetPageSolver,
+    LimitOffsetPageStudy,
+    Solver,
+    Study,
+)
+
+Page = Union[
+    LimitOffsetPageJob, LimitOffsetPageFile, LimitOffsetPageSolver, LimitOffsetPageStudy
+]
+T = TypeVar("T", Job, File, Solver, Study)
 
 
-def _pagination_to_iterator(pagination_method: Callable) -> Iterator[Any]:
-    pass
+def _pagination_to_iterator(
+    pagination_method: Callable[[int, int], Page], limit: int = 20
+) -> Iterator[T]:
+    """Returns an iterator given a pagination entrypoint
+
+    Args:
+        pagination_method (Callable[[int, int], Page]): A lambda which takes as first input the limit and as second input the offset of the page
+        limit (int, optional): Limit of the page. Defaults to 20.
+
+    Raises:
+        StopIteration: Raised when one has iterated through all elements in the iterator
+
+    Yields:
+        Iterator[T]: The returned iterator
+    """
+    offset: int = 0
+    item_count: int = 0
+    while True:
+        page: Page = pagination_method(limit, offset)
+        assert page.items is not None
+        assert isinstance(page.total, int)
+        for item in page.items:
+            item_count += 1
+            yield item
+        offset += limit
+        if item_count >= page.total:
+            raise StopIteration
