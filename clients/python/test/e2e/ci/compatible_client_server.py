c28010f3a80b3ce393fd751db9852fb720fcca65
Mads Bisgaard <bisgaard@itis.swiss>
2023-08-22 10:53:12 +0200

    modify compatibility json and script

--- clients/python/test/e2e/ci/compatible_client_server.py
+++ clients/python/test/e2e/ci/compatible_client_server.py
@@ -22,21 +22,17 @@ def main() -> None:
     server_cfg: ServerConfig = pytest_ini.server
     if not _COMPATIBILITY_JSON.is_file():
         raise typer.Exit(code=E2eExitCodes.CI_SCRIPT_FAILURE)
-    compatibility_df: pd.DataFrame = pd.read_json(_COMPATIBILITY_JSON)
+    df: pd.DataFrame = pd.read_json(_COMPATIBILITY_JSON).T
 
-    if client_cfg.compatibility_ref not in compatibility_df.columns:
-        print(
-            f"The client ref '{client_cfg.compatibility_ref}' could not "
-            f"be found in {_COMPATIBILITY_JSON}"
-        )
-        raise typer.Exit(code=E2eExitCodes.INVALID_CLIENT_VS_SERVER)
-    if server_cfg.url.netloc not in compatibility_df.index:
-        print(
-            f"The server netloc '{server_cfg.url.netloc}' could not "
-            f"be found in {_COMPATIBILITY_JSON}"
-        )
+    try:
+        is_compatible = df[
+            (df["server"] == server_cfg.url.netloc)
+            & (df["client"] == client_cfg.compatibility_ref)
+        ]["is_compatible"]
+    except Exception:
+        raise typer.Exit(code=E2eExitCodes.CI_SCRIPT_FAILURE)
 
-    if not compatibility_df[client_cfg.compatibility_ref][server_cfg.url.netloc]:
+    if not is_compatible:
         raise typer.Exit(code=E2eExitCodes.INVALID_CLIENT_VS_SERVER)
     else:
         raise typer.Exit(code=pytest.ExitCode.OK)
