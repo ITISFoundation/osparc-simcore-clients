66a1e7a8d4ab28e242ef576ca9adb3673c894763
Mads Bisgaard <bisgaard@itis.swiss>
2023-10-05 16:48:41 +0200

    skip notebook tests if not correct versions

--- clients/python/test/e2e/test_notebooks.py
+++ clients/python/test/e2e/test_notebooks.py
@@ -1,14 +1,19 @@
 import shutil
 import sys
 from pathlib import Path
-from typing import Any, List
+from typing import Any, Dict, List
 
 import osparc
 import papermill as pm
 import pytest
+from packaging.version import Version
 
 docs_dir: Path = Path(__file__).parent.parent.parent / "docs"
 all_notebooks: List[Path] = list(docs_dir.rglob("*.ipynb"))
+max_notebook_versions: Dict[str, Version] = {
+    "BasicTutorial_v0.5.0.ipynb": Version("0.5.0"),
+    "BasicTutorial_v0.6.0.ipynb": Version("0.6.0"),
+}
 
 
 def test_notebook_config(tmp_path: Path):
@@ -25,6 +30,15 @@ def test_notebook_config(tmp_path: Path):
         },
     )
     assert len(all_notebooks) > 0, f"Did not find any notebooks in {docs_dir}"
+    max_keys: set = set(max_notebook_versions.keys())
+    notebook_names: set = set(pth.name for pth in all_notebooks)
+    msg: str = (
+        "Must specify max version ",
+        f"for: {notebook_names-max_keys}.",
+        " The following keys can be ",
+        f"deleted: {max_keys - notebook_names}",
+    )
+    assert max_keys == notebook_names, msg
 
 
 @pytest.mark.parametrize("notebook", all_notebooks)
@@ -34,6 +48,12 @@ def test_run_notebooks(tmp_path: Path, notebook: Path, params: dict[str, Any] = 
     assert (
         notebook.is_file()
     ), f"{notebook.name} is not a file (full path: {notebook.resolve()})"
+    if Version(osparc.__version__) > Version(max_notebook_versions[notebook.name]):
+        msg: str = (
+            f"Skipping {notebook.name} because "
+            f"{osparc.__version__=} > {max_notebook_versions[notebook.name]}"
+        )
+        pytest.skip(msg)
     tmp_nb = tmp_path / notebook.name
     shutil.copy(notebook, tmp_nb)
     assert tmp_nb.is_file(), "Did not succeed in copying notebook"
