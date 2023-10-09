aae9ec54a30577b9ce0ba04b4effac462a824968
Mads Bisgaard <bisgaard@itis.swiss>
2023-10-09 13:31:13 +0200

    improve decorator

--- clients/python/test/e2e/_utils.py
+++ clients/python/test/e2e/_utils.py
@@ -2,7 +2,6 @@ import json
 import os
 from pathlib import Path
 
-import decorator
 import osparc
 import pytest
 from packaging.version import Version
@@ -13,22 +12,16 @@ _config: Path = (
 assert _config.is_file()
 
 
+def osparc_dev_features_enabled() -> bool:
+    return os.environ.get("OSPARC_DEV_FEATURES_ENABLED") == "1"
+
+
 def requires_dev_features(test):
     repo_version: Version = Version(
         json.loads(_config.read_text())["python"]["version"]
     )
-
-    @pytest.mark.skipif(
-        Version(osparc.__version__) < repo_version or not osparc_dev_features_enabled(),
-        reason=(
+    if Version(osparc.__version__) < repo_version or not osparc_dev_features_enabled():
+        return pytest.mark.skip(
             f"{osparc.__version__=}<{repo_version=} or {osparc_dev_features_enabled()=}"
-        ),
-    )
-    def wrapper(test, *args, **kwargs):
-        return test(*args, **kwargs)
-
-    return decorator.decorator(wrapper, test)
-
-
-def osparc_dev_features_enabled() -> bool:
-    return os.environ.get("OSPARC_DEV_FEATURES_ENABLED") == "1"
+        )(test)
+    return test
