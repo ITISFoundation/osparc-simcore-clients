c96352ed649d718b218961d2096df327d0954b79
Mads Bisgaard <bisgaard@itis.swiss>
2023-11-02 10:45:43 +0100

    update versioning and github workflow

--- scripts/generate_version.bash
+++ scripts/generate_version.bash
@@ -7,8 +7,11 @@ set -o errexit  # abort on nonzero exitstatus
 set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 
-latest=$(git ls-remote --tags --refs --sort=version:refname https://github.com/ITISFoundation/osparc-simcore-clients \
-  | tail -1 \
-  | grep -oP '(?<=refs/tags/v)\d+\.\d+\.\d+')
-new_version=$(echo "${latest}" | sed -E 's/([0-9]+\.[0-9]+\.)([0-9]+)/echo "\1$((\2+1))"/e')
-echo -n "${new_version}"
+release_info=$(git ls-remote --tags --refs --sort=version:refname https://github.com/ITISFoundation/osparc-simcore-clients | tail -1)
+release_version=$(echo "${release_info}" | grep -oP '(?<=refs/tags/v)\d+\.\d+\.\d+')
+release_commit=$(echo "${release_info}" | grep -oE '^[[:alnum:]]+')
+current_commit=$(git rev-parse HEAD)
+
+merge_base=$(git merge-base "${release_commit}" "${current_commit}")
+n_commits_to_merge_base=$(git rev-list --count "${merge_base}".."${current_commit}")
+echo -n "${release_version}+${n_commits_to_merge_base}"
