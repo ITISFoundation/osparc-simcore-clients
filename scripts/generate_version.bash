0f7db5a67271b791c80ab1ac57b0327fbf4bb8ee
Mads Bisgaard <bisgaard@itis.swiss>
2023-11-06 09:50:24 +0100

    improve error handling and add version file to artifacts

--- scripts/generate_version.bash
+++ scripts/generate_version.bash
@@ -3,9 +3,11 @@
 # Generate version <semantic version of last tag(=release)>+<number of commits from last tagged commit>
 # This is done by inspecting the tags on the git repo https://github.com/ITISFoundation/osparc-simcore-clients
 
-set -o errexit  # abort on nonzero exitstatus
+#set -o errexit  # abort on nonzero exitstatus
 set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
+
+trap "{ echo 'Could not generate a version file. Probably you need to sync your fork.' >&2; }" ERR
 
 release_info=$(git ls-remote --tags --refs --sort=version:refname https://github.com/ITISFoundation/osparc-simcore-clients | tail -1)
 release_version=$(echo "${release_info}" | grep -oP '(?<=refs/tags/v)\d+\.\d+\.\d+')
