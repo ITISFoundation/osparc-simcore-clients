16d1110e733a54afad25ac6ed1809435ef3712e0
Mads Bisgaard <bisgaard@itis.swiss>
2023-12-05 15:44:03 +0100

    simplify client config

--- clients/python/test/e2e/ci/bash/install_osparc_python_client.bash
+++ clients/python/test/e2e/ci/bash/install_osparc_python_client.bash
@@ -7,32 +7,28 @@ set -o pipefail # don't hide errors within pipes
 doc="Install osparc python client\n"
 doc+="Input:\n"
 doc+="------\n"
-doc+="\tA single json string. This json string is expected to be the output of setup_client_config.bash or as logged in the pyproject.toml"
+doc+="\tA single json string containing either the field 'dev_features' as well as 'version'\n"
+doc+="\t'version' must either be a semantic version, equal to 'latest_release' or equal to 'latest_master'. 'dev_features' must be either 'true' or 'false''\n"
+doc+="\tExample: bash install_osparc_python_client.bash '{\"dev_features\": \"true\", \"version\":  \"master\"}'\n"
 
 print_doc() { echo -e "$doc"; }
 [ $# -eq 0 ] && print_doc && exit 0
 
 client_config=$1
 
+if [[ $(echo "$client_config" | jq 'has("version")') != "true" ]]; then
+  echo "'version' key was missing"
+fi
+version=$(echo "${client_config}" | jq -r .version)
+if [[ $(echo "$client_config" | jq 'has("dev_features")') != "true" ]]; then
+  echo "'dev_features' key was missing"
+fi
 
-
-
-if [[ $(echo "$client_config" | jq 'has("client_version")') == "true" ]]; then
-  client_version=$(echo "${client_config}" | jq -r .client_version)
-  v_string=""
-  if [[ "${client_version}" != "latest" ]]; then
-    v_string="==${client_version}"
-  fi
-  python -m pip install osparc"${v_string}" --force-reinstall
+if [[ "${version}" == "master" ]]; then
+  python -m pip install osparc --extra-index-url https://test.pypi.org/simple/ --force-reinstall
 else
-  client_repo=$(echo "${client_config}" | jq -r .client_repo)
-  client_runid=$(echo "${client_config}" | jq -r .client_runid)
-  tmp_dir=$(mktemp -d)
-  pushd "${tmp_dir}"
-  echo "gh run download ${client_runid} --repo=${client_repo}"
-  gh run download "${client_runid}" --repo="${client_repo}"
-  popd
-  osparc_wheel=$(ls "${tmp_dir}"/osparc_python_wheels/osparc-*.whl)
-  python -m pip install "${osparc_wheel}" --find-links="${tmp_dir}"/osparc_python_wheels --force-reinstall
-  rm -rf "${tmp_dir}"
+  if [[ "${version}" != "latest" ]]; then
+    v_string="==${version}"
+  fi
+    python -m pip install osparc"${v_string}" --force-reinstall
 fi
