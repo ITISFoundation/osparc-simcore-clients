453a2ac71e2d2b3de287fbff68404714d5fde2d2
Mads Bisgaard <bisgaard@itis.swiss>
2024-02-23 14:55:39 +0100

    add retry

--- clients/python/client/osparc/_http_client.py
+++ clients/python/client/osparc/_http_client.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Any, Awaitable, Callable, Optional
 
 import httpx
 import tenacity
+from osparc_client import Configuration
 
 
 class AsyncHttpClient:
@@ -10,16 +11,18 @@ class AsyncHttpClient:
     def __init__(
         self,
         *,
+        configuration: Configuration,
         request_type: Optional[str] = None,
         url: Optional[str] = None,
         **httpx_async_client_kwargs
     ):
+        self.configuration = configuration
         self._client = httpx.AsyncClient(**httpx_async_client_kwargs)
         self._callback = getattr(self._client, request_type) if request_type else None
         self._url = url
 
-    async def __aenter__(self) -> httpx.AsyncClient:
-        return self._client
+    async def __aenter__(self) -> "AsyncHttpClient":
+        return self
 
     async def __aexit__(self, exc_type, exc_value, traceback) -> None:
         if exc_value is None:
@@ -41,3 +44,42 @@ class AsyncHttpClient:
                     raise err from exc_value
             await self._client.aclose()
             raise exc_value
+
+    async def _request(
+        self, method: Callable[[Any], Awaitable[httpx.Response]], *args, **kwargs
+    ) -> httpx.Response:
+        async for attempt in tenacity.AsyncRetrying(
+            reraise=True,
+            wait=tenacity.wait_exponential(
+                multiplier=self.configuration.retries.backoff_factor
+            ),
+            stop=tenacity.stop_after_attempt(
+                self.configuration.retries.total
+                if isinstance(self.configuration.retries.total, int)
+                else 4
+            ),
+            retry=tenacity.retry_if_exception_type(httpx.HTTPStatusError),
+        ):
+            with attempt:
+                response: httpx.Response = await method(*args, **kwargs)
+                try:
+                    response.raise_for_status()
+                except httpx.HTTPStatusError as exc:
+                    if (
+                        exc.response.status_code
+                        in self.configuration.retries.status_forcelist
+                    ):
+                        raise exc
+                return response
+
+    async def put(self, *args, **kwargs) -> httpx.Response:
+        return await self._request(self._client.put, *args, **kwargs)
+
+    async def post(self, *args, **kwargs) -> httpx.Response:
+        return await self._request(self._client.post, *args, **kwargs)
+
+    async def delete(self, *args, **kwargs) -> httpx.Response:
+        return await self._request(self._client.delete, *args, **kwargs)
+
+    async def patch(self, *args, **kwargs) -> httpx.Response:
+        return await self._request(self._client.patch, *args, **kwargs)
