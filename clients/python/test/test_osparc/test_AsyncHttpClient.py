74b930c01f2b482f023c2c206a2b0d67f40b4bd3
Mads Bisgaard <bisgaard@itis.swiss>
2024-03-21 16:16:22 +0100

    start adding test

--- clients/python/test/test_osparc/test_AsyncHttpClient.py
+++ clients/python/test/test_osparc/test_AsyncHttpClient.py
@@ -1,6 +1,7 @@
 import httpx
 import osparc
 import pytest
+import respx
 from osparc._http_client import AsyncHttpClient
 
 
@@ -50,3 +51,21 @@ def test_retry_strategy(cfg: osparc.Configuration, fake_retry_state):
         )
         == 15
     )
+
+
+async def test_aexit(cfg: osparc.Configuration, respx_mock: respx.Mock):
+    _call_url: str = "5b0c5cb6-5e88-479c-a54d-2d5fa39aa97b"
+    _exit_url: str = "43c2fdfc-690e-4ba9-9ae7-55a911c159d0"
+
+    def _side_effect(status_code: int, request: httpx.Request):
+        return httpx.Response(status_code=status_code)
+
+    respx_mock.get(_call_url).mock(side_effect=_side_effect)
+
+    async with AsyncHttpClient(
+        configuration=cfg,
+        request_type="get",
+        url="79ae41cc-0d89-4714-ac9d-c23ee1b110ce",
+        body={"this": "field"},
+    ):
+        pass
