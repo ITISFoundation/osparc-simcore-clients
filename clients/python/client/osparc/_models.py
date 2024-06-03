e3973d20310f57d432c76dd8c656a821872087b6
Mads Bisgaard <bisgaard@itis.swiss>
2024-06-03 16:16:25 +0200

    add parent info in solversapi

--- clients/python/client/osparc/_models.py
+++ clients/python/client/osparc/_models.py
@@ -8,5 +8,7 @@ class ParentProjectInfo(BaseSettings):
     """Information a project cann pass onto its "children" (i.e. projects
     'spawned' through the api-server)"""
 
-    parent_project_uuid: UUID | None = Field(alias="OSPARC_NODE_ID", default=None)
-    parent_node_id: UUID | None = Field(alias="OSPARC_STUDY_ID", default=None)
+    x_simcore_parent_project_uuid: UUID | None = Field(
+        alias="OSPARC_NODE_ID", default=None
+    )
+    x_simcore_parent_node_id: UUID | None = Field(alias="OSPARC_STUDY_ID", default=None)
