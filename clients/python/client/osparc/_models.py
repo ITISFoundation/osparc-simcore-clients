c5424ceb6d5af4fd88c689612174d637fd9c4ad3
Mads Bisgaard <bisgaard@itis.swiss>
2024-06-05 10:34:56 +0200

    attempt to make code py 3.8 compatible

--- clients/python/client/osparc/_models.py
+++ clients/python/client/osparc/_models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from uuid import UUID
 
 from pydantic import Field, field_validator
@@ -8,10 +9,12 @@ class ParentProjectInfo(BaseSettings):
     """Information a project cann pass onto its "children" (i.e. projects
     'spawned' through the api-server)"""
 
-    x_simcore_parent_project_uuid: str | None = Field(
+    x_simcore_parent_project_uuid: Optional[str] = Field(
         alias="OSPARC_STUDY_ID", default=None
     )
-    x_simcore_parent_node_id: str | None = Field(alias="OSPARC_NODE_ID", default=None)
+    x_simcore_parent_node_id: Optional[str] = Field(
+        alias="OSPARC_NODE_ID", default=None
+    )
 
     @field_validator("x_simcore_parent_project_uuid", "x_simcore_parent_node_id")
     @classmethod
