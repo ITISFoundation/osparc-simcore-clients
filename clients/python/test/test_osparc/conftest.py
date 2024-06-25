2f6528c5a533fd7330fb357f1c51f55fc67734f1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-06-25 11:52:40 +0200

    annotate

--- clients/python/test/test_osparc/conftest.py
+++ clients/python/test/test_osparc/conftest.py
@@ -1,18 +1,16 @@
-from typing import Iterator
-
 import osparc
 import pytest
-
+from faker import Faker
 
 @pytest.fixture
-def cfg() -> Iterator[osparc.Configuration]:
-    yield osparc.Configuration(
+def cfg(faker: Faker) -> osparc.Configuration:
+    return osparc.Configuration(
         host="https://1a52d8d7-9f9f-48e5-b2f0-a226e6b25f0b.com",
-        username="askjdg",
-        password="asdjbaskjdb",
+        username=faker.user_name(),
+        password=faker.password(),
     )
 
 
 @pytest.fixture
-def enable_dev_mode(monkeypatch):
+def enable_dev_mode(monkeypatch:pytest.MonkeyPatch):
     monkeypatch.setenv("OSPARC_DEV_FEATURES_ENABLED", "1")
