5b62e53597c5974599dbb6e6a7bfdc13bf41ae84
Mads Bisgaard <bisgaard@itis.swiss>
2024-06-26 09:48:04 +0200

    minor changes

--- clients/python/client/osparc/_files_api.py
+++ clients/python/client/osparc/_files_api.py
@@ -119,7 +119,7 @@ class FilesApi(_FilesApi):
             configuration=self.api_client.configuration, timeout=timeout_seconds
         ) as session:
             with logging_redirect_tqdm():
-                _logger.info("Uploading %s in %i chunks", file.name, n_urls)
+                _logger.info("Uploading %s in %i chunk(s)", file.name, n_urls)
                 async for chunck, size in tqdm(
                     file_chunk_generator(file, chunk_size),
                     total=n_urls,
