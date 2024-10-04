0f97ff6d8622fba4195bb7d24a35ddbe0de2e2da
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-04 17:04:02 +0200

    exceptions

--- clients/python/src/osparc/_exceptions.py
+++ clients/python/src/osparc/_exceptions.py
@@ -1,7 +1,12 @@
 from functools import wraps
 
 from httpx import HTTPStatusError
-from osparc_client import ApiException
+
+from osparc_client.exceptions import OpenApiException as OpenApiException
+from osparc_client.exceptions import ApiTypeError as ApiTypeError
+from osparc_client.exceptions import ApiValueError as ApiValueError
+from osparc_client.exceptions import ApiKeyError as ApiKeyError
+from osparc_client.exceptions import ApiException as ApiException
 
 
 class VisibleDeprecationWarning(UserWarning):
