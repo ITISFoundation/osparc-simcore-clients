883747209882e84c673974b35094c1299ed9488b
Mads Bisgaard <bisgaard@itis.swiss>
2024-10-24 16:12:45 +0200

    improve JobInputs constructor

--- clients/python/src/osparc/models.py
+++ clients/python/src/osparc/models.py
@@ -79,12 +79,29 @@ from typing import Any, Dict
 TaskStates = _RunningState
 
 
+def _values_dict(v: Dict[str, Any]) -> Dict[str, ValuesValue | None]:
+    result = {}
+    for k, v in v.items():
+        if v is not None:
+            result[k] = ValuesValue(v)
+        else:
+            result[k] = v
+    return result
+
+
 class JobInputs(_JobInputs):
     def __init__(self, *args, **kwargs):
         if len(args) == 1 and len(kwargs) == 0:
             input = args[0]
             assert isinstance(input, dict)
-            super().__init__(values={k: ValuesValue(v) for k, v in input.items()})
+            super().__init__(values=_values_dict(input))
+            return
+        if len(args) == 0 and len(kwargs) == 1:
+            values = kwargs.get("values")
+            if values is None:
+                raise RuntimeError("When passing a single kwarg it must be 'values'")
+            super().__init__(values=_values_dict(values))
+            return
         else:
             super().__init__(*args, **kwargs)
 
